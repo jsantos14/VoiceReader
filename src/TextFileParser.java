25e957b95b7f3186cf8feb6ef8dcb1aaecc42c8f
Joe Santos <jsantos14@georgefox.edu>
2017-09-01 20:04:35 -0700

    Adding some project settings.

--- src/TextFileParser.java
+++ src/TextFileParser.java
@@ -1,9 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 public class TextFileParser
 {
-    public static ArrayList<String> parseText (String filePath)
+    public static ArrayList<String> parseText (String filePath) throws
+            FileNotFoundException
     {
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+        
+
         ArrayList<String> words = new ArrayList<>();
 
         return words;
