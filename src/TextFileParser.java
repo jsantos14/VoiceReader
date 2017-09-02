fc66a2e6863bea04151a8882cded2c01c5bb0d11
Joe Santos <jsantos14@georgefox.edu>
2017-09-01 19:36:40 -0700

    Starter

---
+++ src/TextFileParser.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class TextFileParser
+{
+    public static ArrayList<String> parseText (String filePath)
+    {
+        ArrayList<String> words = new ArrayList<>();
+
+        return words;
+    }
+}
