6bd19cfa70f466fc1c9a469724d785e1e6a6e502
Joe Santos <jsantos14@georgefox.edu>
2017-09-04 12:33:24 -0700

    Reformatting code.

--- src/SoundClipTest.java
+++ src/SoundClipTest.java
@@ -3,41 +3,53 @@
 import java.io.*;
 import javax.sound.sampled.*;
 import javax.swing.*;
-   
+
 // To play sound using Clip, the process need to be alive.
 // Hence, we use a Swing application.
 @SuppressWarnings("serial")
-public class SoundClipTest extends JFrame {
-   
-   // Constructor
-   public SoundClipTest() {
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      this.setTitle("Test Sound Clip");
-      this.setSize(300, 200);
-      this.setVisible(true);
-   
-      try {
-         // Open an audio input stream.
-         File soundFile = new File("C:\\Users\\Ryan\\Dropbox\\Junior Fall Semester\\Eclipse\\Audio\\DeJager.wav");
-         AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+public class SoundClipTest extends JFrame
+{
 
-         // Get a sound clip resource.
-         Clip clip = AudioSystem.getClip();
-         
-         // Open audio clip and load samples from the audio input stream.
-         clip.open(audioIn);
-         clip.start();
-         
-      } catch (UnsupportedAudioFileException e) {
-         e.printStackTrace();
-      } catch (IOException e) {
-         e.printStackTrace();
-      } catch (LineUnavailableException e) {
-         e.printStackTrace();
-      }
-   }
-   
-   public static void main(String[] args) {
-      new SoundClipTest();
-   }
+    // Constructor
+    public SoundClipTest()
+    {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Test Sound Clip");
+        this.setSize(300, 200);
+        this.setVisible(true);
+
+        try
+        {
+            // Open an audio input stream.
+            File soundFile = new File(
+                    "C:\\Users\\Ryan\\Dropbox\\Junior Fall "
+                            + "Semester\\Eclipse\\Audio\\DeJager.wav");
+            AudioInputStream audioIn =
+                    AudioSystem.getAudioInputStream(soundFile);
+
+            // Get a sound clip resource.
+            Clip clip = AudioSystem.getClip();
+
+            // Open audio clip and load samples from the audio input stream.
+            clip.open(audioIn);
+            clip.start();
+        }
+        catch (UnsupportedAudioFileException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        catch (LineUnavailableException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main( String[] args )
+    {
+        new SoundClipTest();
+    }
 }
